9600b281e88797ae2a03f40b19683ee44c7c37c2
damiakadiri <amosakadiri2@gmail.com>
2023-09-06 17:24:19 +0100

    2nd commit

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const main = document.getElementById("main");
-
-function fetchData() {
-  fetch("https://bayut.p.rapidapi.com/properties/detail?externalID=4937770", {
+fetch(
+  "https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4",
+  {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "5e4f4e2f06msh2dcc16334e634eep12ff8bjsn74d01f6b0b0f",
+      "X-RapidAPI-Key": "cbf2168d07msh6f9a02c23429942p1a2153jsndff097ebfc34",
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response.title);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+  }
+)
+  .then((response) => response.json())
+  .then((data) => {
+    const list = data.hits;
 
-fetchData();
+    list.map((item) => {
+      const title = item.title;
+      const price = item.price;
+      const location = item.location[0].name;
+      const image = item.coverPhoto.url;
+      const property = `<li><img src="${image}" <h2>${title}</h2><h3>${price}DH</h3><p>${location}</p></li>`;
+      document.querySelector(".properties").innerHTML += property;
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
