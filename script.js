38c6eac99bc541888a8f6bebb7a4004d7b5d7fbe
damiakadiri <amosakadiri2@gmail.com>
2023-09-29 12:28:46 +0100

    refactored the javascript

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-fetch(
-  "https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4",
-  {
+const properties = document.querySelector(".properties");
+const url =
+  "https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=4";
+
+async function getProperty() {
+  const response = await fetch(url, {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "cbf2168d07msh6f9a02c23429942p1a2153jsndff097ebfc34",
+      "X-RapidAPI-Key": "a2d143a19fmsha27797a43c69b44p15349fjsn4770dbbe4007",
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
-  }
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const list = data.hits;
-
-    list.map((item) => {
-      const title = item.title;
-      const price = item.price;
-      const location = item.location[0].name;
-      const image = item.coverPhoto.url;
-      const property = `<li><img src="${image}" <h2>${title}</h2><h3>${price}DH</h3><p>${location}</p></li>`;
-      document.querySelector(".properties").innerHTML += property;
-    });
-  })
-  .catch((error) => {
-    console.error("Error:", error);
   });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const data = await response.json(); // Extracting data as a JSON Object from the response
+  showProperties(data.hits);
+}
+
+getProperty();
+
+function showProperties(property) {
+  properties.innerHTML = "";
+  property.forEach((item) => {
+    const { title, price, location } = item;
+    const image = item.coverPhoto.url;
+
+    const propertyEl = document.createElement("div");
+    propertyEl.classList.add("property");
+
+    propertyEl.innerHTML = `
+
+     <img
+       src="${image}"
+       alt="house"
+      />
+      <h3>${price}</h3>
+      <p>${title}</p>
+
+
+    `;
+    properties.appendChild(propertyEl);
+  });
+}
